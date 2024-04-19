829d8f086aa8979f3f8c16d0d6e018c850d32820
TheGreatGreeble <73497579+TheGreatGreeble@users.noreply.github.com>
2024-04-18 21:31:40 -0700

    tweaked peashooter

--- Assets/Scripts/WeaponSwapper.cs
+++ Assets/Scripts/WeaponSwapper.cs
@@ -11,24 +11,26 @@ public class WeaponSwapper : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        launcher1.SetActive(false);
+        launcher1.SetActive(true);
         launcher2.SetActive(false);
+        peaShooter1.SetActive(false);
+        peaShooter2.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space)) {
-            if (launcher1.activeInHierarchy == false) {
-                peaShooter1.SetActive(false);
+            if (launcher1.activeInHierarchy) {
+                peaShooter1.SetActive(true);
                 peaShooter2.SetActive(false);
-                launcher1.SetActive(true);
+                launcher1.SetActive(false);
                 launcher2.SetActive(true);
             }
             else {
-                peaShooter1.SetActive(true);
+                peaShooter1.SetActive(false);
                 peaShooter2.SetActive(true);
-                launcher1.SetActive(false);
+                launcher1.SetActive(true);
                 launcher2.SetActive(false);
             }
         }
