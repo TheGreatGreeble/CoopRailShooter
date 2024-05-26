e47f4cc97d5c248f149b88e80c5e75168631703e
meowmers321 <mrmeowmers321@gmail.com>
2024-05-26 15:06:36 -0700

    Character controller now MUCH smoother, updated mouse turn on-er script to a public bool

--- Assets/Scripts/MovementManager.cs
+++ Assets/Scripts/MovementManager.cs
@@ -6,32 +6,37 @@ public class MovementManager : MonoBehaviour
 {
     [SerializeField] public GameObject Player1;
     [SerializeField] public GameObject Player2;
-    [SerializeField] float speed;
+    [SerializeField] float speed = 10f;
+    [SerializeField] float acceleration = 2f;
     private CharacterController characterControllerP1;
     private CharacterController characterControllerP2;
-    private float verticalVelocity = 0.0f;
+    private Vector3 currentVelocityP1 = Vector3.zero;
+    private Vector3 currentVelocityP2 = Vector3.zero;
     
     // Start is called before the first frame update
     void Start()
     {
         characterControllerP1 = Player1.GetComponent<CharacterController>();
         characterControllerP2 = Player2.GetComponent<CharacterController>();
-        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //player 1 move calculation
-        Vector3 moveP1 = Player1.transform.right * Input.GetAxis("Horizontal");
-        //player 2 move calculation
-        Vector3 moveP2 = Player2.transform.right * Input.GetAxis("Vertical");
-        if (!characterControllerP1.isGrounded || !characterControllerP2.isGrounded)
-        {
-            verticalVelocity -= (10 * Time.deltaTime);
-        }
+        // Player 1 move calculation
+        Vector3 inputDirectionP1 = Player1.transform.right * Input.GetAxis("Horizontal");
 
-        characterControllerP1.Move((moveP1 + verticalVelocity * Vector3.up) * Time.deltaTime * speed);
-        characterControllerP2.Move((moveP2 + verticalVelocity * Vector3.up) * Time.deltaTime * speed);
+        // Accelerate Player 1 towards desired velocity
+        currentVelocityP1 = transform.TransformVector(Vector3.MoveTowards(currentVelocityP1, inputDirectionP1, acceleration * Time.deltaTime));
+
+        // Player 2 move calculation
+        Vector3 inputDirectionP2 = Player2.transform.right * Input.GetAxis("Vertical");
+
+        // Accelerate Player 2 towards desired velocity
+        currentVelocityP2 = transform.TransformVector(Vector3.MoveTowards(currentVelocityP2, inputDirectionP2, acceleration * Time.deltaTime));
+
+        // Move the players
+        characterControllerP1.Move(currentVelocityP1 * speed * Time.deltaTime);
+        characterControllerP2.Move(currentVelocityP2 * speed * Time.deltaTime);
     }
 }
--- Assets/Scripts/mouseturnoner.cs
+++ Assets/Scripts/mouseturnoner.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class mouseturnoner : MonoBehaviour
 {
+    public bool on;
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.None;
+        if (on) {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
     }
 
     // Update is called once per frame
